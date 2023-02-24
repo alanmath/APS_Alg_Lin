a1c2bee4bf7ac35de8abd3a64b14240b15fa852b
esdrasgc <esdrasgcdev@gmail.com>
2023-02-24 20:42:34 -0300

    correção de bug do buraco de minhoca (buraco indo para frente)

--- sprites/wormhole.py
+++ sprites/wormhole.py
@@ -5,26 +5,20 @@ from sprites.snake import Cobra
 class Wormhole:
     lista = []
 
-    def __init__(self, x, y, indice_buraco_ligado):
-        self.pos = [x, y]
-        self.rect = pygame.Rect(x, y, WORMHOLE_WIDTH, WORMHOLE_HEIGHT)
-        self.indice = indice_buraco_ligado
-        Wormhole.lista.append(self)
-
-
         # get the image of the wormhole
     def __init__(self, valores):
-        self.pos = [valores[0], valores[1]]
+        self.pos = (valores[0], valores[1])
         self.rect = pygame.Rect(valores[0], valores[1], WORMHOLE_WIDTH, WORMHOLE_HEIGHT)
         self.indice = valores[2]
+        self.image = pygame.image.load("sprites/portal.png")
         Wormhole.lista.append(self)
-        self.wormhole = pygame.image.load("sprites/portal.png")
+        
 
     @classmethod
     def draw(cls, screen):
         for wormhole in Wormhole.lista:
             #draw wormhole
-            screen.blit(wormhole.wormhole, (wormhole.pos[0], wormhole.pos[1]))
+            screen.blit(wormhole.image, (wormhole.pos[0], wormhole.pos[1]))
 
 
     @classmethod
@@ -37,6 +31,5 @@ class Wormhole:
             for cobra in Cobra.lista:
                 if wormhole.rect.colliderect(cobra.rect):
                     pos = Wormhole.lista[wormhole.indice].pos
-                    pos[0] += (WORMHOLE_WIDTH + SNAKE_WIDTH + 4) / 2
-                    # pos[1] += (WORMHOLE_HEIGHT + SNAKE_HEIGHT + 10) / 2
-                    cobra.transportar(pos)
+                    pos_x = pos[0] + ((WORMHOLE_WIDTH + SNAKE_WIDTH + 4) / 2)
+                    cobra.transportar((pos_x, pos[1]))
